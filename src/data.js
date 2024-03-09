fa789956c2c3e9e5018882117bee63d8f9eb039f
Saad bajwa <saadbajwa659@gmail.com>
2024-03-09 15:26:14 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,80 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+    return [
+        
+        {
+            name: "Beaver Creek",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            artist: "Aso, Middle School, Aviino",
+            audio: "https://stream.chillhop.com/mp3/10076",
+            color: ['#205950', '#2ab3bf'],
+            id: uuidv4(),
+            active: true,
+        },
+        {
+            name: "Daylight",
+            cover: "https://chillhop.com/wp-content/uploads/2023/05/a553b95f790409f69feabdd6279f1153a11f14b3-1024x1024.jpg",
+            artist: "Shofel",
+            audio: "https://stream.chillhop.com/mp3/9272",
+            color: ['#EF8EA9', '#ac417f'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Keep Going",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            artist: "Swørn",
+            audio: "https://stream.chillhop.com/mp3/9222",
+            color: ['#CD607D', '#c94043'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Nightfall",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+            artist: "Aiguille",
+            audio: "https://stream.chillhop.com/mp3/9148",
+            color: ['#EF8EA9', '#ab417f'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Reflection",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            artist: "Swørn",
+            audio: "https://stream.chillhop.com/mp3/9228",
+            color: ['#CD607D', '#c94043'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Under the City Stars",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            artist: " Aso, Middle School, Aviino ",
+            audio: "https://stream.chillhop.com/mp3/10074",
+            color: ['#B1403C', '#FBEEDB'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Jazz Cabbage",
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+            artist: "Ian Ewing, Strehlow",
+            audio: "https://stream.chillhop.com/mp3/9363",
+            color: ['#B1403C', '#FBEEDB'],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Lagoons",
+            artist: "Strehlow, Chris Mazuera",
+            id: uuidv4(),
+            active: false,
+            color: ['#B1403C', '#FBEEDB'],
+            cover: "https://chillhop.com/wp-content/uploads/2023/09/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+            audio: "https://stream.chillhop.com/mp3/8266",
+        },
+    ]
+}
+
+export default chillHop;
