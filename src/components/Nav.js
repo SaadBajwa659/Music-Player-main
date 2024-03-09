04f29c130c6c45dfcaa31155f8491cf2c33e8bf5
Saad bajwa <saadbajwa659@gmail.com>
2024-03-09 15:27:56 +0500

    added faM

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faM, faMusic } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = ({setLibraryStatus, libraryStatus}) => {
     return (
